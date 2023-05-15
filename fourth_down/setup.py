03a36cb999826ab27f5ed82e7501391770442088
Lee davies <lee.davies@trayport.com>
2023-05-15 22:42:57 +0100

    authors

--- fourth_down/setup.py
+++ fourth_down/setup.py
@@ -5,7 +5,7 @@ py_modules = ["fourth_down_api"]
 setup(
     packages=py_modules,
     name="fourth_down_api",
-    author="Lee and stolen Titusz code",
+    author=["Lee Davies", "Titusz Ban"],
     version="1.0.0",
     description="Fourth down API client",
     install_requires=[
