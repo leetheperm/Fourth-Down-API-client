de1c41cdb0ac6bb276d37e3cf3becdebfdf4fab4
Lee Davies <leetheperm@gmail.com>
2023-11-13 23:05:13 +0000

    add try catch for non 200 requests

--- fourth_down/fourth_down_api/api_base.py
+++ fourth_down/fourth_down_api/api_base.py
@@ -22,15 +22,19 @@ class ApiBase:
 
     def _get(self, request: ApiRequest) -> str:
         start_time = perf_counter()
+
         r = requests.get(
             f"{self._endpoint}/{request.path.lstrip('/')}",
             params=request.params,
             headers={**request.headers})
         duration = perf_counter() - start_time
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            return "Error: " + str(e)
         if request.verbose:
             print(f"Request url: {r.url}")
             print(f"Duration: {duration:.3f}s")
             print(f"Status: {r.status_code}")
-            r.raise_for_status()
             return r.text
 
